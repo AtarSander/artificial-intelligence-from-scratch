2288524319469d6518d8a6e8e1ae0fc97f90a63a
Aleksander Szymczyk <aleksander.szymczyk.stud@pw.edu.pl>
2023-10-28 18:58:21 +0200

    Implemented 3d plotting (without gradient steps yet)

--- Zad1/plotter.py
+++ Zad1/plotter.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -29,7 +29,7 @@ def plotter_1D(func, domain, grad_x=None, grad_y=None, num_points=400,
     plt.show()
 
 
-def plotter_2D(func, grad_x=None, domain: List[Tuple] = [(-10, 10), (-10, 10)],  num_points=400,
+def plotter_2D(func, domain=[(-10, 10), (-10, 10)], grad_x=None, num_points=400,
                title=None, x1_label='x1', x2_label='x2', legend_label=None, steps=False):
     #TODO: add two options of showing arrows (every arrow vs every so steps)
     x1 = np.linspace(domain[0][0], domain[0][1], num_points)
@@ -52,4 +52,26 @@ def plotter_2D(func, grad_x=None, domain: List[Tuple] = [(-10, 10), (-10, 10)], 
     if legend_label:
         plt.legend(legend_label)
 
+    plt.show()
+
+
+def plotter_3D(func, domain, grad_x=None, num_points=100, title=None,
+               view=(15, -30), x1_label='x1', x2_label='x2',
+               x3_label='f(x1, x2)', legend_label=None, steps=False):
+    x1 = np.linspace(domain[0][0], domain[0][1], num_points)
+    x2 = np.linspace(domain[1][0], domain[1][1], num_points)
+    X1, X2 = np.meshgrid(x1, x2)
+    Z = func((X1, X2))
+    # fig = plt.figure(figsize=[12, 8])
+    ax = plt.axes(projection="3d")
+    ax.plot_surface(X1, X2, Z, cmap="plasma")
+    ax.view_init(*view)
+
+    plt.xlabel(x1_label)
+    plt.ylabel(x2_label)
+    # plt.zlabel(x3_label)
+    if title:
+        plt.title(title)
+    if legend_label:
+        plt.legend(legend_label)
     plt.show()
