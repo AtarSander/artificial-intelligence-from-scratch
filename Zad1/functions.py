326c184a0a70198d294275390d844e6fee4672d6
Aleksander Szymczyk <aleksander.szymczyk.stud@pw.edu.pl>
2023-10-29 13:22:43 +0100

    Added documentation to functions.py

--- Zad1/functions.py
+++ Zad1/functions.py
@@ -2,19 +2,23 @@ import numpy as np
 
 
 def f1(x):
-    return 1/4 * x**4
+    """A mathematical function"""
+    return np.array(1/4 * np.power(x, 4))
 
 
 def gradient_f1(x):
-    return x**3
+    """A mathematical function gradient"""
+    return np.array(np.power(x, 3))
 
 
 def f2(x):
+    """A mathematical function"""
     x1, x2 = x
-    return 1.5 - np.exp(-x1**2 - x2**2) - 0.5 * np.exp(-(x1 - 1)**2 - (x2 + 2)**2)
+    return np.array(1.5 - np.exp(-x1**2 - x2**2) - 0.5 * np.exp(-(x1 - 1)**2 - (x2 + 2)**2))
 
 
 def gradient_f2(x0):
+    """A mathematical function gradient"""
     x1, x2 = x0
     return np.array([2 * x1 * np.exp(-x1**2 - x2**2) + (x1 - 1) * np.exp(-(x1 - 1)**2 - (x2 + 2)**2),
                      2 * x2 * np.exp(-x1**2 - x2**2) + (x2 + 2) * np.exp(-(x1 - 1)**2 - (x2 + 2)**2)])
