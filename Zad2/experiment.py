1ceff5a3fbd0ef160d97fb2993c1e7797d3ead2d
atarsander <szymczyk.alek1@gmail.com>
2023-11-09 16:51:55 +0100

    Implemented experimenting only on the same budget

--- Zad2/experiment.py
+++ Zad2/experiment.py
@@ -1,17 +1,23 @@
 from genetic import Genetic
 from itertools import product
 from physics import target_function
-import numpy as np
 
 
-def experiment(ind_num, iter_nums, cross_rate, mut_rate):
-    combinations = list(product(ind_num, iter_nums, cross_rate, mut_rate))
+def experiment(ind_num, iter_nums, cross_rate, mut_rate, budget):
+    ind_and_iter = []
+    for i in ind_num:
+        for j in iter_nums:
+            if i*j == budget:
+                ind_and_iter.append((i, j))
+
+    combinations = list(product(ind_and_iter, cross_rate, mut_rate))
     solution = Genetic()
+    g = []
     for combo in combinations:
-        g = []
         o = 0
         for _ in range(25):
-            x_best, o_best = solution.solve(target_function, combo[0], combo[1], combo[2], combo[3])
+            x_best, o_best = solution.solve(target_function, combo[0][0],
+                                            combo[0][1], combo[1], combo[2])
             o += o_best
         o /= 25
         g.append(o)
