e9008a7cad7ed541ca360a324f27e05bb5e93ee9
atarsander <szymczyk.alek1@gmail.com>
2023-11-29 17:48:38 +0100

    Added enum class for options, and functions for playing with algorithm interactively

--- Zad3/experiment.py
+++ Zad3/experiment.py
@@ -3,17 +3,23 @@ import sys
 import random
 import numpy as np
 import copy
-sys.path.append("two-player-games")
+from enum import IntEnum
+sys.path.append("Zad3/two-player-games")
 from two_player_games.games.connect_four import ConnectFour
 
 
-def experiment(type, games_num, ab_turn=None, depths=None):
+class Option(IntEnum):
+    RAND_VS_RAND = 1
+    ALGO_VS_RAND = 2
+    RAND_VS_ALGO = 3
+    ALGO_VS_ALGO = 4
+    ALGO_VS_HUMAN = 5
+    HUMAN_VS_ALGO = 6
+
+
+def experiment(option, games_num, depths=None):
     results = {}
-    if depths is not None:
-        results["Number of games"] = [games_num for _ in range(len(depths))]
-    else:
-        results["Number of games"] = [games_num]
-    if type == 1:
+    if option == 1:
         player_1_wins = 0
         player_2_wins = 0
         draws = 0
@@ -32,11 +38,13 @@ def experiment(type, games_num, ab_turn=None, depths=None):
                 player_1_wins += 1
             else:
                 player_2_wins += 1
+
+        results["Number of games"] = [games_num]
         results["Player 1 wins"] = [player_1_wins]
         results["Player 2 wins"] = [player_2_wins]
         results["Draws"] = [draws]
 
-    elif type == 2 and depths and ab_turn:
+    elif option == 2 or option == 3:
         player_1_wins_depth = []
         player_2_wins_depth = []
         draws_depth = []
@@ -48,7 +56,7 @@ def experiment(type, games_num, ab_turn=None, depths=None):
                 game = ConnectFour()
                 ab_player = Alphabeta(depth)
 
-                winner = ab_game(game, ab_player, ab_turn, depth)
+                winner = ab_game(game, ab_player, option, depth)
 
                 if winner is None:
                     draws += 1
@@ -60,19 +68,53 @@ def experiment(type, games_num, ab_turn=None, depths=None):
             player_1_wins_depth.append(player_1_wins)
             player_2_wins_depth.append(player_2_wins)
             draws_depth.append(draws)
+        results["Number of games"] = [games_num for _ in range(len(depths))]
         results["Player 1 wins"] = player_1_wins_depth
         results["Player 2 wins"] = player_2_wins_depth
         results["Draws"] = draws_depth
         results["Depths"] = depths
 
+    elif option == 4:
+        player_1_wins_depth = []
+        player_2_wins_depth = []
+        draws_depth = []
+        for depth1 in depths:
+            for depth2 in depths:
+                player_1_wins = 0
+                player_2_wins = 0
+                draws = 0
+                for _ in range(games_num):
+                    game = ConnectFour()
+                    ab_player = Alphabeta(depth1)
+
+                    winner = ab_game(game, ab_player, option, depth1, depth2)
+
+                    if winner is None:
+                        draws += 1
+                    elif winner.char == '1':
+                        player_1_wins += 1
+                    else:
+                        player_2_wins += 1
+
+                player_1_wins_depth.append(player_1_wins)
+                player_2_wins_depth.append(player_2_wins)
+                draws_depth.append(draws)
+        results["Number of games"] = [games_num for _ in range(len(depths) * len(depths))]
+        results["Player 1 wins"] = player_1_wins_depth
+        results["Player 2 wins"] = player_2_wins_depth
+        results["Draws"] = draws_depth
+        results["Depths player 1"] = [depth for depth in depths for _ in range(len(depths))]
+        results["Depths player 2"] = depths * len(depths)
+
     else:
-        raise AttributeError
+        raise AttributeError("Wrong option chosen")
 
     return results
 
 
-def ab_game(game, ab_player, who, depth):
-    if who == 1:
+def ab_game(game, ab_player, who, depth, depth2=None):
+    # Algo vs rand
+    if who == 2:
         max_player = game.get_current_player()
         while not game.is_finished():
             temp_game = copy.deepcopy(game)
@@ -84,7 +126,7 @@ def ab_game(game, ab_player, who, depth):
                 game.make_move(move_rand)
         winner = game.get_winner()
 
-    elif who == 2:
+    elif who == 3:
         max_player = game.get_current_player()
         while not game.is_finished():
             moves = game.get_moves()
@@ -96,7 +138,8 @@ def ab_game(game, ab_player, who, depth):
                 game.make_move(move_ab)
         winner = game.get_winner()
 
-    elif who == 3:
+    # Algo vs algo
+    elif who == 4:
         max_player = game.get_current_player()
         while not game.is_finished():
             temp_game = copy.deepcopy(game)
@@ -104,9 +147,72 @@ def ab_game(game, ab_player, who, depth):
             game.make_move(move_ab)
             if not game.is_finished():
                 temp_game = copy.deepcopy(game)
-                move_ab = ab_player.solve(temp_game, depth, -np.inf, np.inf, max_player)[0]
+                move_ab = ab_player.solve(temp_game, depth2, -np.inf, np.inf, max_player)[0]
                 game.make_move(move_ab)
         winner = game.get_winner()
+
+    # Human vs player
+    elif who == 5:
+        max_player = game.get_current_player()
+        while not game.is_finished():
+            temp_game = copy.deepcopy(game)
+            move_ab = ab_player.solve(temp_game, depth, -np.inf, np.inf, max_player)[0]
+            game.make_move(move_ab)
+            if not game.is_finished():
+                chosen = -1
+                print(game)
+                move = int(input("Choose column: "))
+                moves = game.get_moves()
+                for i in moves:
+                    if i.column == move:
+                        chosen = i
+                if chosen != -1:
+                    game.make_move(chosen)
+                else:
+                    raise AttributeError("Wrong move chosen")
+        print(game)
+        winner = game.get_winner()
+
+    elif who == 6:
+        max_player = game.get_current_player()
+        while not game.is_finished():
+            chosen = -1
+            print(game)
+            move = int(input("Choose column: "))
+            moves = game.get_moves()
+            for i in moves:
+                if i.column == move:
+                    chosen = i
+            if chosen != -1:
+                game.make_move(chosen)
+            else:
+                raise AttributeError("Wrong move chosen")
+            if not game.is_finished():
+                temp_game = copy.deepcopy(game)
+                move_ab = ab_player.solve(temp_game, depth, -np.inf, np.inf, max_player)[0]
+                game.make_move(move_ab)
+        print(game)
+        winner = game.get_winner()
     else:
-        raise AttributeError
+        raise AttributeError("Wrong option chosen")
     return winner
+
+
+def play_game(option, depth):
+    game = ConnectFour()
+    ab_player = Alphabeta(depth)
+    if option == 5 or option == 6:
+        winner = ab_game(game, ab_player, option, depth)
+        if winner is None:
+            result = "It's a draw!"
+        elif winner.char == '1' and option == 5:
+            result = "You lose!"
+        elif winner.char == '2' and option == 5:
+            result = "You win!"
+        elif winner.char == '1' and option == 6:
+            result = "You win!"
+        else:
+            result = "You lose!"
+    else:
+        raise AttributeError("Wrong option chosen")
+    return result
