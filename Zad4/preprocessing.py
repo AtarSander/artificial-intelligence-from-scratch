deba45f409e3c6d74ae564e7ed1360c54c00f8fb
atarsander <szymczyk.alek1@gmail.com>
2023-12-13 11:38:31 +0100

    Added wider age discretion

--- Zad4/preprocessing.py
+++ Zad4/preprocessing.py
@@ -1,14 +1,16 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def set_discrete_columns(df, columns):
     intervals = {}
     for column in columns:
         numbered_intervals = {}
-        num_bins = int(np.sqrt(len(df[column].unique())))
+        if column == "age":
+            num_bins = int(np.sqrt(np.sqrt(len(df[column].unique()))))
+        else:
+            num_bins = int(np.sqrt(len(df[column].unique())))
         temp_intervals = pd.cut(df[column], bins=num_bins).unique().tolist()
         sorted_intervals = sorted(
             [(interval.mid, interval) for interval in temp_intervals]
@@ -33,7 +35,7 @@ def split_dataset(df, target):
     X = df.drop(target, axis=1)
     y = df[target]
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, stratify=y, random_state=42
+        X, y, test_size=0.3, stratify=y, random_state=42
     )
     X_dev, X_test, y_dev, y_test = train_test_split(
         X_test, y_test, test_size=0.5, stratify=y_test, random_state=42
