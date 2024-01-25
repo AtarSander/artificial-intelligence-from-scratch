c26a3b1fad4282c1c67f5bcd8dbfd100cd2c56a5
atarsander <szymczyk.alek1@gmail.com>
2024-01-25 06:04:16 +0100

    added function for splitting data

--- Zad7/preprocessing.py
+++ Zad7/preprocessing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 
 
 def remove_outliers(df, columns, n_std):
@@ -28,3 +31,21 @@ def set_discrete_columns(df, columns):
         intervals[column] = numbered_intervals
         df[column] = pd.cut(df[column], bins=num_bins, labels=False)
     return df, intervals
+
+
+def split_data(dataset, target, test_size=0.2, validation_size=0.2, random_state=42):
+    X_train, X_temp, y_train, y_temp = train_test_split(
+        dataset.drop(columns=[target]),
+        dataset[target],
+        test_size=test_size + validation_size,
+        random_state=random_state,
+    )
+
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_temp,
+        y_temp,
+        test_size=test_size / (test_size + validation_size),
+        random_state=random_state,
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
