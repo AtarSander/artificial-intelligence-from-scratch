8cdffe1a30b8728ff852b353afe194499f501b69
atarsander <szymczyk.alek1@gmail.com>
2024-02-21 19:45:52 +0100

    in task 1: changed folder name, changed language of report to english

---
+++ gradient_descent_optimization/functions.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def f1(x, threshold=100):
+    """A mathematical function"""
+    if np.any(np.abs(x)) > threshold:
+        return np.inf
+    else:
+        return np.array(1/4 * np.power(x, 4))
+
+
+def gradient_f1(x, threshold=100):
+    """A mathematical function gradient, returns inf if gradient explodes"""
+    if np.any(np.abs(x)) > threshold:
+        return np.inf
+    else:
+        return np.array(np.power(x, 3))
+
+
+def f2(x):
+    """A mathematical function"""
+    x1, x2 = x
+    return np.array(1.5 - np.exp(-x1**2 - x2**2) - 0.5 * np.exp(-(x1 - 1)**2 - (x2 + 2)**2))
+
+
+def gradient_f2(x0):
+    """A mathematical function gradient"""
+    x1, x2 = x0
+    return np.array([2 * x1 * np.exp(-x1**2 - x2**2) + (x1 - 1) * np.exp(-(x1 - 1)**2 - (x2 + 2)**2),
+                     2 * x2 * np.exp(-x1**2 - x2**2) + (x2 + 2) * np.exp(-(x1 - 1)**2 - (x2 + 2)**2)])
