ffab6100ac1c83004345ae138351eb8ae6a2c57c
atarsander <szymczyk.alek1@gmail.com>
2024-12-10 01:40:48 +0100

    Started work on layer class.
    - implemented separate layer for clean forward and back propagation
    - modified model to implement this layers
    - enables bigger modularity of created models

---
+++ Zad5/layer.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+
+class Layer:
+    def __init__(self, layer_dims, activation):
+        self.weights, self.bias = self.initialize_parameters()
+        self.activation = activation.lower()
+
+    def initialize_parameters(self):
+        pass
+
+    def forward(self, X):
+        Z = self.forward_linear(X)
+        Z = self.activation(Z)
+        return Z
+
+    def forward_linear(self, X):
+        w, b = self.weights, self.bias
+        Z = np.dot(np.transpose(X), w) + b
+        return Z
+
+    def activation(self, Z):
+        if self.activation == "relu":
+            Z = self.ReLU(Z)
+        elif self.activation == "softmax":
+            Z = self.SoftMax(Z)
+        return Z
+
+    def backward(self, d_prev):
+        dZ = d_prev * self.cache["activation"]
+        dW = np.dot(dZ, np.transpose(self.weight))
+
+
+    def ReLU(self, Z):
+        self.cache["activation"] = np.ones(Z.shape)
+        return np.maximum(Z, 0)
+
+    def SoftMax(self, x):
+        shift_x = x-np.max(x)
+        e_x = np.exp(shift_x)
+        softmax = e_x / np.sum(e_x)
+        self.cache["activation"] = softmax * (1-softmax)
+        return softmax
+
+    def update_patameters(self, grads_w, grads_b, learning_rate):
+        self.weights = self.weights - learning_rate * grads_w
+        self.bias = self.bias - learning_rate * grads_b
+
+
+
+
+
--- Zad5/model.py
+++ Zad5/model.py
@@ -6,12 +6,8 @@ class Model:
     def __init__(self):
         self.layers = []
 
-    def initialize_parameters(self):
-        pass
-
-    def add_module(self, name, module):
-        if not (hasattr(module, "__self__") and isinstance(module.__self__, self)):
-            raise ValueError
+    def add_module(self, layer):
+        self.layers.append(layer)
 
     def forward_propagation(self, X):
         A_prev = X
@@ -22,11 +18,3 @@ class Model:
     def backward_propagation(self, x):
         pass
 
-    def ReLU(self, Z):
-        cache = Z
-        return np.maximum(Z, 0), cache
-
-    def SoftMax(self, x):
-        sigma = 1/(1+np.exp(x))
-        d_sigma = sigma * (1-sigma)
-        return sigma, d_sigma
