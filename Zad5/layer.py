ccb6472bf215e2f1357b7425784c3aa6cd9ec54a
atarsander <szymczyk.alek1@gmail.com>
2024-12-13 02:12:17 +0100

    Working prototype of multi-layer perceptron in numpy.

--- Zad5/layer.py
+++ Zad5/layer.py
@@ -3,47 +3,51 @@ import numpy as np
 
 class Layer:
     def __init__(self, layer_dims, activation):
-        self.weights, self.bias = self.initialize_parameters(layer_dims)
-        self.activation = activation.lower()
+        self.initialize_parameters(layer_dims)
+        self.activation_type = activation.lower()
+        self.cache = {}
 
     def initialize_parameters(self, dimensions):
-        self.weights = np.random.normal(0, 2 / dimensions[1], dimensions)
+        self.weights = np.array(np.random.normal(0, 2 / dimensions[1], list(dimensions)[::-1]))
         self.bias = np.zeros((dimensions[1],1))
 
     def forward(self, X):
+        self.cache["input"] = X
         Z = self.forward_linear(X)
         Z = self.activation(Z)
         return Z
 
     def forward_linear(self, X):
         w, b = self.weights, self.bias
-        Z = np.dot(np.transpose(X), w) + b
+        Z = np.dot(w, X) + b
         return Z
 
     def activation(self, Z):
-        if self.activation == "relu":
-            Z = self.ReLU(Z)
-        elif self.activation == "softmax":
-            Z = self.SoftMax(Z)
+        if self.activation_type == "relu":
+            Z = self.relu(Z)
+        elif self.activation_type == "softmax":
+            Z = self.softmax(Z)
         return Z
 
-    def backward(self, d_prev, A_prev):
+    def backward(self, d_prev):
         dZ = d_prev * self.cache["activation"]
-        dW = np.dot(dZ, np.transpose(A_prev))
+        dW = np.dot(dZ, np.transpose(self.cache["input"]))
         db = np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(np.transpose(self.weights), dZ)
+        return dW, db, dA_prev
 
-    def ReLU(self, Z):
+    def relu(self, Z):
         self.cache["activation"] = np.ones(Z.shape)
         return np.maximum(Z, 0)
 
-    def SoftMax(self, x):
+    def softmax(self, x):
         shift_x = x-np.max(x)
         e_x = np.exp(shift_x)
         softmax = e_x / np.sum(e_x)
         self.cache["activation"] = softmax * (1-softmax)
         return softmax
 
-    def update_patameters(self, grads_w, grads_b, learning_rate):
+    def update_parameters(self, grads_w, grads_b, learning_rate):
         self.weights = self.weights - learning_rate * grads_w
         self.bias = self.bias - learning_rate * grads_b
 
--- Zad5/model.py
+++ Zad5/model.py
@@ -1,5 +1,4 @@
 import numpy as np
-from layer import Layer
 
 
 class Model:
@@ -12,9 +11,32 @@ class Model:
     def forward_propagation(self, X):
         A_prev = X
         for layer in self.layers:
-            A, cache = layer.forward(A_prev)
+            A = layer.forward(A_prev)
+            A_prev = A
+        AL = A_prev
+        return AL
 
+    def calculate_loss(self, y_pred, y_truth):
+        epsilon = 1e-8
+        CE = -np.sum(np.multiply(y_truth, np.log(y_pred+epsilon)))
+        return CE
 
-    def backward_propagation(self, x):
-        pass
+    def backward_propagation(self, AL, Y):
+        dA_prev = AL - Y
+        for layer in reversed(self.layers):
+            dW, db, dA_prev = layer.backward(dA_prev)
+            layer.update_parameters(dW, db, self.learning_rate)
+
+    def train(self, X, Y, epochs, learning_rate):
+        self.learning_rate = learning_rate
+        for epoch in range(epochs):
+            y_pred = self.forward_propagation(X)
+            loss = self.calculate_loss(y_pred, Y)
+            self.backward_propagation(y_pred, Y)
+            if epoch % 10 == 0:
+                print(f"Loss: {loss}")
+
+    def predict(self, X):
+        y_pred = self.forward_propagation(X)
+        return y_pred
 
