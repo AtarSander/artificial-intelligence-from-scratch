d93849de332c95c6a4b6e8392c08bf60ca3c4177
atarsander <aleksander.szymczyk.stud@pw.edu.pl>
2025-03-22 00:45:00 +0100

    Implemented new function for doing the experiments regarding nn architecture and updated data preprocessing to have correct types and formatting

--- Zad5/experiment.py
+++ Zad5/experiment.py
@@ -1,56 +1,66 @@
-from model import Model
+from model import Model, train
+from layer import Layer
+from preprocessing import MnistDataloader
+from PIL import Image as im
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def experiment(list_of_layers, learning_rates, data, batch_sizes, num_epochs):
-    costs = []
-    results = {}
-    train_accuracy_column = []
-    dev_accuracy_column = []
-    max_accuracy = 0
-    X_train, Y_train, X_dev, Y_dev = data
-    for i in range(len(learning_rates)):
-        new_model = Model(list_of_layers[i])
-        cost = new_model.train(
-            X_train,
-            Y_train,
-            learning_rates[i],
-            num_epochs[i],
-            batch_sizes[i],
-            1,
-            False,
+def experiment(
+    data_train: tuple[np.ndarray, np.ndarray],
+    data_val: tuple[np.ndarray, np.ndarray],
+    list_of_layers: list[list[tuple[tuple[int, int]], str]],
+    num_epochs: int,
+    learning_rates: list[float],
+    batch_sizes: list[int],
+) -> tuple[list[list[float]], list[float], list[float]]:
+    X_train, y_train = data_train
+    X_val, y_val = data_val
+    losses = []
+    train_accuracies = []
+    val_accuracies = []
+    for layers, learning_rate, batch_size in zip(
+        list_of_layers, learning_rates, batch_sizes
+    ):
+        model = Model()
+        for layer in layers:
+            model.add_module(Layer(layer[0], layer[1]))
+        model_losses = train(
+            model, X_train, y_train, num_epochs, learning_rate, batch_size
         )
-        costs.append(cost)
-        acc_dev = new_model.predict(X_dev, Y_dev)
-        acc_trn = new_model.predict(X_train, Y_train)
-        train_accuracy_column.append(acc_trn)
-        dev_accuracy_column.append(acc_dev)
-        if acc_dev > max_accuracy:
-            max_accuracy = acc_dev
-            best_model = new_model
-    results["Number of epochs"] = num_epochs
-    results["Batch size"] = batch_sizes
-    results["Learning rate"] = learning_rates
-    results["Layers"] = list_of_layers
-    results["Train accuracy"] = train_accuracy_column
-    results["Dev accuracy"] = dev_accuracy_column
+        losses.append(model_losses)
+        train_accuracy = measure_accuracy(model, X_train, y_train)
+        val_accuracy = measure_accuracy(model, X_val, y_val)
+        train_accuracies.append(train_accuracy)
+        val_accuracies.append(val_accuracy)
+    return losses, train_accuracies, val_accuracies
 
-    return results, costs, best_model
+
+def measure_accuracy(model: Model, x: np.ndarray, y: np.ndarray) -> float:
+    y_pred = model.predict(x)
+    y_pred = np.argmax(y_pred, axis=1)
+    accuracy = np.sum(y_pred == y) / len(y)
+    return accuracy.item()
+
+
+def view_image(image: np.ndarray) -> None:
+    data = im.fromarray(image)
+    data.save("test.png")
 
 
 def plot_costs(costs):
-    x_values_modified = [x * 100 for x in range(len(costs[0]))]
+    x_values_modified = [x for x in range(len(costs[0]))]
     for ind, cost in enumerate(costs):
         plt.plot(
             x_values_modified,
             cost,
-            marker="o",
+            marker="",
             linestyle="-",
             label=f"Model number: {ind}",
         )
 
     plt.xlabel("Epochs")
-    plt.ylabel("Cost")
+    plt.ylabel("Loss")
     plt.title("Costs of models in training")
     plt.legend()
     plt.show()
--- Zad5/preprocessing.py
+++ Zad5/preprocessing.py
@@ -1,40 +1,72 @@
 import numpy as np
 import struct
 from array import array
-from os.path  import join
+from os.path import join
+
 
 class MnistDataloader(object):
-    def __init__(self, training_images_filepath,training_labels_filepath,
-                 test_images_filepath, test_labels_filepath):
+    def __init__(
+        self,
+        training_images_filepath: str,
+        training_labels_filepath: str,
+        test_images_filepath: str,
+        test_labels_filepath: str,
+        validation_size: int = 10000,
+    ) -> None:
         self.training_images_filepath = training_images_filepath
         self.training_labels_filepath = training_labels_filepath
         self.test_images_filepath = test_images_filepath
         self.test_labels_filepath = test_labels_filepath
+        self.validation_size = validation_size
 
-    def read_images_labels(self, images_filepath, labels_filepath):
+    def read_images_labels(
+        self, images_filepath: str, labels_filepath: str
+    ) -> tuple[np.ndarray, np.ndarray]:
         labels = []
-        with open(labels_filepath, 'rb') as file:
+        with open(labels_filepath, "rb") as file:
             magic, size = struct.unpack(">II", file.read(8))
             if magic != 2049:
-                raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
+                raise ValueError(
+                    "Magic number mismatch, expected 2049, got {}".format(magic)
+                )
             labels = array("B", file.read())
 
-        with open(images_filepath, 'rb') as file:
+        with open(images_filepath, "rb") as file:
             magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
             if magic != 2051:
-                raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
+                raise ValueError(
+                    "Magic number mismatch, expected 2051, got {}".format(magic)
+                )
             image_data = array("B", file.read())
         images = []
         for i in range(size):
             images.append([0] * rows * cols)
         for i in range(size):
-            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
+            img = np.array(image_data[i * rows * cols : (i + 1) * rows * cols])
             img = img.reshape(28, 28)
             images[i][:] = img
 
-        return images, labels
+        return np.array(images), np.array(labels)
 
-    def load_data(self):
-        x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
-        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
-        return (x_train, y_train),(x_test, y_test)
+    def load_data(
+        self,
+    ) -> tuple[
+        tuple[np.ndarray, np.ndarray],
+        tuple[np.ndarray, np.ndarray],
+        tuple[np.ndarray, np.ndarray],
+    ]:
+        x_train, y_train = self.read_images_labels(
+            self.training_images_filepath, self.training_labels_filepath
+        )
+        x_val, y_val = (
+            x_train[: self.validation_size],
+            y_train[: self.validation_size],
+        )
+        x_train, y_train = (
+            x_train[self.validation_size :],
+            y_train[self.validation_size :],
+        )
+        x_test, y_test = self.read_images_labels(
+            self.test_images_filepath, self.test_labels_filepath
+        )
+        return (x_train, y_train), (x_val, y_val), (x_test, y_test)
